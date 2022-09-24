b5a0f4942f3c2491b91fa74ff0aba6446cba4ce4
saad-out <outsaad03@gmail.com>
2022-09-24 10:45:58 +0000

    print 'None' in case of empty list

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    for i in range(len(my_list) - 1, -1, -1):
-        print("{:d}".format(my_list[i]))
+    size = len(my_list)
+    if size == 0:
+        print("None")
+    else:
+        for i in range((size - 1), -1, -1):
+            print("{:d}".format(my_list[i]))
