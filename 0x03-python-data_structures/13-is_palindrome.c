2cbbbea452618518a925de525ce25cee41fd4d09
saad-out <outsaad03@gmail.com>
2022-09-24 21:15:56 +0000

    allocate right amount of memory + check for NULL node

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,6 +1,27 @@
 #include "lists.h"
 
 /**
+ * allocate_memory - allocate required memory for an array
+ * @head: head node of listint_t
+ *
+ * Return: allocated array
+ */
+int *allocate_memory(listint_t *head)
+{
+	int *arr;
+	size_t size;
+
+	size = 0;
+	while (head)
+		size++, head = head->next;
+
+	arr = malloc(sizeof(int) * size);
+	if (!arr)
+		exit(1);
+	return (arr);
+}
+
+/**
  * is_palindrome - checks if a list is palindrom
  * @head: pointer to head node of list
  *
@@ -9,11 +30,12 @@
 int is_palindrome(listint_t **head)
 {
 	listint_t *tmp;
-	int i, values[1000];
+	int i, *values;
 
-	if (!(*head))
+	if (!head || !(*head))
 		return (1);
 
+	values = allocate_memory(*head);
 	tmp = *head, i = -1;
 	while (tmp)
 	{
@@ -25,9 +47,13 @@ int is_palindrome(listint_t **head)
 	for (; i >= 0; i--)
 	{
 		if (values[i] != tmp->n)
+		{
+			free(values);
 			return (0);
+		}
 		tmp = tmp->next;
 	}
 
+	free(values);
 	return (1);
 }
