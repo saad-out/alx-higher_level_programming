ddbde21ede9910fbc4f42940d83cde3f988cd918
saad-out <outsaad03@gmail.com>
2022-09-26 23:31:40 +0000

    place code inside if statement

--- 0x03-python-data_structures/100-print_python_list_info.c
+++ 0x03-python-data_structures/100-print_python_list_info.c
@@ -1,29 +1,23 @@
-#include <Python.h>
+#include "Python.h"
 
-/**
- * print_python_list_info - prints some basic info about Python lists
- * @p: python object
- *
- * Return: void
- */
 void print_python_list_info(PyObject *p)
 {
 	PyListObject *list;
-	PyObject *obj;
 	Py_ssize_t size, i;
+	PyObject *object;
 	struct _typeobject *type;
 
-	if (strcmp(p->ob_type->tp_name, "list") != 0)
-		return;
-
-	list = (PyListObject *)p;
-	size = list->ob_base.ob_size;
-	printf("[*] Size of the Python List = %ld\n", size);
-	printf("[*] Allocated = %ld\n", list->allocated);
-	for (i = 0; i < size; i++)
+	if (strcmp(p->ob_type->tp_name, "list") == 0)
 	{
-		obj = list->ob_item[i];
-		type = obj->ob_type;
-		printf("Element [%ld]: %s\n", i, type->tp_name);
+		list = (PyListObject *)p;
+		size = list->ob_base.ob_size;
+		printf("[*] Size of the Python List = %ld\n", size);
+		printf("[*] Allocated = %ld\n", list->allocated);
+		for (i = 0; i < size; i++)
+		{
+			object = list->ob_item[i];
+			type = object->ob_type;
+			printf("Element %ld: %s\n", i, type->tp_name);
+		}
 	}
 }
