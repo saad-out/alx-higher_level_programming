0ebf17cc02440505d86b80c8836fe5aab8237cf9
saad-out <outsaad03@gmail.com>
2022-09-28 13:09:04 +0000

    handle wrong type argument

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,7 +2,7 @@
 def roman_to_int(roman_string):
 
     # check if not empty and is sring #
-    if roman_string is None:
+    if (roman_string is None) or (isinstance(roman_string, str) is False):
         return 0
 
     romans = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
