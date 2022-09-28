159ab96a3936e6ea6cd3fbdb5ded5eccba925a7e
saad-out <outsaad03@gmail.com>
2022-09-28 10:06:19 +0000

    delete blank lines

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_matrix = []
-
     if matrix:
         for l in matrix:
             new_list = [x**2 for x in l]
             new_matrix.append(new_list)
-
     return new_matrix
