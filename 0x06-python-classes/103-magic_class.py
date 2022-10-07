88aeab0024ff0f8a1a85d2cc2afd7735c7bd3ef6
saad-out <outsaad03@gmail.com>
2022-10-07 17:25:35 +0000

    use 'and' instead of 'or' when checking type

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -10,7 +10,7 @@ class MagicClass:
 
     def __init__(self, radius):
         self.__radius = 0
-        if type(radius) is not int or type(radius) is not float:
+        if (type(radius) is not int) and (type(radius) is not float):
             raise TypeError("radius must be a number")
         else:
             self.__radius = radius
