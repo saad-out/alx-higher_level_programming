6850b8a221fc88348603ee1e0516adac0c80bd18
saad-out <outsaad03@gmail.com>
2022-10-24 11:52:02 +0000

    unique __str__ for Square

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This is '11-square' module.
+Functions and Classes:
+    class Square
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """represent a square"""
+    def __init__(self, size):
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """string representation of a Square object"""
+        return "[Square] {}/{}".format(self.__size, self.__size)
