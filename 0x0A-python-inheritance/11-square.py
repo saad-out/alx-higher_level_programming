759da183ce43a0167fec2edafb954b058fa5d9b2
saad-out <outsaad03@gmail.com>
2022-10-24 11:59:22 +0000

    function spelling mistake

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -12,7 +12,7 @@ Rectangle = __import__('9-rectangle').Rectangle
 class Square(Rectangle):
     """represent a square"""
     def __init__(self, size):
-        self.intger_validator("size", size)
+        self.integer_validator("size", size)
         super().__init__(size, size)
         self.__size = size
 
