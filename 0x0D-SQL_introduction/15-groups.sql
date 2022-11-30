f61cd480118e6beb9f5082a6a9803da65058568e
saad-out <outsaad03@gmail.com>
2022-11-30 09:03:34 +0000

    use GROUP BY and ORDER BY

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,4 +1,5 @@
 -- list number of similar score records
-SELECT score, COUNT(score) AS "number"
+SELECT score, COUNT(score) AS number
 FROM second_table
-GROUP BY score;
+GROUP BY score
+ORDER BY number DESC;
