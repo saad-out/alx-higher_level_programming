d4abb6605368eed0c654d7c28941303b16601e98
saad-out <outsaad03@gmail.com>
2022-11-30 14:47:03 +0000

    states table

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- create a database and add a table to it
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+	id INT NOT NULL AUTO_INCREMENT,
+	name VARCHAR(256),
+	PRIMARY KEY(id)
+);
