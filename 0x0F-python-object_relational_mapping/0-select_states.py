d367c9b46cc0cae00e4025bd148de7af17eeaaec
saad-out <outsaad03@gmail.com>
2022-12-21 12:33:54 +0000

    should not be executed when imported

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -3,18 +3,27 @@
 This script that lists all states from the database hbtn_0e_0_usa
 """
 
-import MySQLdb
-from sys import argv
+
+def main():
+    """
+    List 'states' table of 'hbtn_0e_0_usa' database in ascending
+    order by id's
+    """
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host='localhost', port=3306,
+                         user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id;")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+
+    cur.close()
+    db.close()
 
 
-db = MySQLdb.connect(host='localhost', port=3306,
-                     user=argv[1], passwd=argv[2], db=argv[3])
-cur = db.cursor()
-
-cur.execute("SELECT * FROM states ORDER BY id;")
-rows = cur.fetchall()
-for row in rows:
-    print(row)
-
-cur.close()
-db.close()
+if __name__ == '__main__':
+    main()
