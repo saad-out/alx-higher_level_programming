71b5b51bc0dc78bdaf7fcb11d170d2e842fe2efc
saad-out <outsaad03@gmail.com>
2022-12-22 20:57:25 +0000

    First state

---
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This module lists first state from 'hbtn_0e_6_usa' database
+"""
+
+
+def main():
+    """
+    List first of 'States' table objects in 'hbtn_0e_usa' database
+    """
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import create_engine
+    from sys import argv
+
+    from model_state import Base, State
+
+    cnx = "mysql+mysqldb://{}:{}@localhost/{}".format(str(argv[1]),
+                                                      str(argv[2]),
+                                                      str(argv[3]))
+    engine = create_engine(cnx)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state = session.query(State).first()
+    print("{}: {}".format(state.id, state.name))
+
+
+if __name__ == '__main__':
+    main()
