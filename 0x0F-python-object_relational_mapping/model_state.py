2309459605e17395e40445b3cd0549d44cad769f
saad-out <outsaad03@gmail.com>
2022-12-22 19:12:41 +0000

    First model state

---
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+This module declares the first state model
+"""
+
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+Base = declarative_base()
+
+
+class State(Base):
+    """
+    State class linking to 'states' MySQL table
+    """
+
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False)
+    name = Column(String(128), nullable=False)
