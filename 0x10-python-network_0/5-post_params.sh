a7a02ceb0acaa04cff49f70d436040e028a4a46e
saad-out <outsaad03@gmail.com>
2023-01-16 15:07:07 +0000

    -s flag in all bash scripts

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # This script takes in a URL, sends a POST request to the passed URL, and displays the body of the response
-curl -X POST "$1" -d 'email="test@gmail.com"' -d 'subject="I will always be here for PLD"'
+curl -sX POST "$1" -d 'email="test@gmail.com"' -d 'subject="I will always be here for PLD"'
