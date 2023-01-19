ca658f8de70bd8540992cb129db3e4867f66a625
saad-out <outsaad03@gmail.com>
2023-01-19 14:49:36 +0000

    handle edge cases

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -4,21 +4,23 @@ This module contains a function to return peek of a list
 """
 
 
-def find_peak(int_l):
+def find_peak(ints):
     """
     finds a peak in a list of unsorted integers
     """
-    if not int_l or type(int_l) is not list:
+    if not ints or type(ints) is not list:
         return None
 
-    left, right = 0, len(int_l) - 1
+    left, right = 0, len(ints) - 1
     while left < right:
         mid = left + (right - left) // 2
-        if mid < len(int_l) and int_l[mid] < int_l[mid + 1]:
-            left = mid + 1
-        elif mid >= 0 and int_l[mid] < int_l[mid - 1]:
+        condition_1 = (mid - 1 < 0 or ints[mid - 1] <= ints[mid])
+        condition_2 = (mid + 1 >= len(ints) or ints[mid + 1] <= ints[mid])
+        if condition_1 and condition_2:
+            return ints[mid]
+        elif mid - 1 >= 0 and ints[mid - 1] > ints[mid]:
             right = mid - 1
-        else:
-            return int_l[mid]
+        elif mid + 1 < len(ints) and ints[mid + 1] > ints[mid]:
+            left = mid + 1
 
-    return left
+    return ints[left]
