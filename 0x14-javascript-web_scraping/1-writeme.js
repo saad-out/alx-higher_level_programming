0fa7b905b7b07fc9cbac23aa6325df1252837de8
saad-out <outsaad03@gmail.com>
2023-02-09 10:52:20 +0000

    add encoding

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const args = require('process').argv;
 const fs = require('fs');
 
 if (args[2] && args[3]) {
-  fs.writeFile(args[2], args[3], err => {
+  fs.writeFile(args[2], args[3], { encoding: 'utf8' }, err => {
     if (err) console.log(err);
   });
 }
