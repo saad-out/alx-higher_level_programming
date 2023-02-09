ccbe2eb6d45a3ad90ee438a463d149c63bed0aec
saad-out <outsaad03@gmail.com>
2023-02-09 10:47:45 +0000

    remove 'utf8' arg

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,7 +3,7 @@ const args = require('process').argv;
 const fs = require('fs');
 
 if (args[2] && args[3]) {
-  fs.writeFile(args[2], args[3], 'utf8', err => {
+  fs.writeFile(args[2], args[3], err => {
     if (err) console.log(err);
   });
 }
