9661cbec784648bb095ba94fe04efbfcc61223b0
saad-out <outsaad03@gmail.com>
2023-02-09 10:54:49 +0000

    remove conditional statement

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -2,8 +2,6 @@
 const args = require('process').argv;
 const fs = require('fs');
 
-if (args[2] && args[3]) {
-  fs.writeFile(args[2], args[3], { encoding: 'utf8' }, err => {
-    if (err) console.log(err);
-  });
-}
+fs.writeFile(args[2], args[3], { encoding: 'utf8' }, err => {
+  if (err) console.log(err);
+});
